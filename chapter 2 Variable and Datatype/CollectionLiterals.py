51a6a5ee18a9b220a2a28a365473a4d09ec9844f
rishisingh <rishicoding9838@gmail.com>
2025-06-06 19:43:05 +0530

    Basic Python

---
+++ chapter 2 Variable and Datatype/CollectionLiterals.py
@@ -0,0 +1,25 @@
+# Collection literals in Python
+# List literals
+'''address = ['Scientech Easy', 'Joraphatak Road', 'Dhanbad']
+print(address)'''
+
+
+# Dictionary literals
+'''fruits_dict = {'a': 'apple',
+               'o': 'orange',
+               'b': 'banana'}
+print(fruits_dict)'''
+
+
+# tuple literals
+'''even_numbers = (2, 4, 6, 8, 10, 12)
+vowels = ('a','e','i','o','u')
+direction = ('North', 'South', 'East', 'West')
+print(even_numbers)
+print(vowels)
+print(direction)'''
+
+
+# Set literals
+fruits = {'Apple', 'Mango', 'Banana', 'Orange', 'Grape'}
+print(fruits)
